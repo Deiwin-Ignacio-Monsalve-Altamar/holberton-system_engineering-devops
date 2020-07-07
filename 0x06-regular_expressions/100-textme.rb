8a6b26b6889fdfc4117d50c253eeb76cda71cd49
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-07-07 01:54:53 +0000

    his exercise was prepared for you by Guillaume Plessis, VP of Infrastructure at TextMe. It is something he uses daily. You can thank Guillaume for his project on Twitter.
    For this task, you’ll be taking over Guillaume’s responsibilities: one afternoon, a T

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,1 +1,5 @@
 #!/usr/bin/env ruby
+variable_from = ARGV[0].scan(/from:(.\w+)/).join
+variable_to = ARGV[0].scan(/to:(.\d+)/).join
+variable_flags = ARGV[0].scan(/flags:(.+-\d)/).join
+puts "#{variable_from},#{variable_to},#{variable_flags}"
