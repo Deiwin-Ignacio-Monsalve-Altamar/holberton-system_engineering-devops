480126a183e0a9eda365ce6e6aeb5ae7fc9ab2a1
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-04 11:00:14 -0500

    add and update files

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 #Let’s practice using Puppet to make changes
-file { 'config'
-path    => '~/.ssh/',
-mode    =>  '0600',
-content => 'Host ubuntu@35.196.97.61
-    HostName ubuntu@35.196.97.61
-    User ubuntu
-    IdentityFile ~/.ssh/holberton
-    PasswordAuthentication no'
+file_line { 'IdentityFile'
+  ensure => present,
+  path   => '/tmp/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton'
+}
+file_line { 'PasswordAuthentication'
+  ensure => present,
+  path   => '/tmp/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
 }
