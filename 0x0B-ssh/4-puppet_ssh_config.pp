8dfee99722c61c2cc68e9d41ade31b158c3f4d35
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-04 11:04:14 -0500

    add and update files

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 #Let’s practice using Puppet to make changes
-file_line { 'IdentityFile'
+file_line { 'IdentityFile':
   ensure => present,
   path   => '/tmp/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/holberton',
   match  => '^IdentityFile'
 }
-file_line { 'PasswordAuthentication'
+file_line { 'PasswordAuthentication':
   ensure => present,
   path   => '/tmp/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
