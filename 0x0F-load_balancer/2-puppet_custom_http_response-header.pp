d293075f61a73964976eff7004e35e53a3514632
Deiwin-Ignacio-Monsalve-Altamar <deiwin1987@gmail.com>
2020-08-14 19:00:42 -0500

    add tasks

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,21 +1,17 @@
-# install nginx
-package { 'nginx':
-  ensure   => 'latest',
-  name     => 'nginx',
-  provider => 'apt',
-}
+#Nginx server so that /redirect_me is redirecting to another page
+class nginx {
+  package { 'nginx':
+    ensure => installed,
+  }
 
+  service { 'nginx':
+    ensure => running,
+    require => Package['nginx'],
+  }
 
-file_line { 'custom_header':
-  path    => '/etc/nginx/sites-available/default',
-  line    => "\tadd_header X-Served-By \$hostname;",
-  after   => '^server {$',
-  require => Package['nginx'],
-}
-
-# start server.
-
-service { 'nginx':
-  ensure => running,
-  enable => true,
+  file_line { 'custom_header':
+    path    => '/etc/nginx/sites-available/default',
+    line    => "\tadd_header X-Served-By \$hostname;",
+    match   => '^server {$',
+    match_for_absence => true,
 }
